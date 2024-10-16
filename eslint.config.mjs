68b0a31935e72ce992af7894a68caabb866b03a3
Boris J. Guzitajevas <borisrunfast@gmail.com>
2024-10-16 20:28:09 +0200

    Testing pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,17 @@
+import globals from 'globals';
 import pluginJs from "@eslint/js";
 import prettier from "eslint-config-prettier";
 
 export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, 
+        ...globals.node,     
+      },
+    },
+  },
+  
   pluginJs.configs.recommended,
   prettier,
 ];
