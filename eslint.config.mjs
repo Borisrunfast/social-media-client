2d2daadd1164545e1d5b378e9bc9d92f6eb26f16
Boris J. Guzitajevas <borisrunfast@gmail.com>
2024-10-16 19:52:53 +0200

    Testing pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,7 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import prettierConfig from "eslint-config-prettier"; // Import Prettier config to disable conflicting rules
-import prettierPlugin from "eslint-plugin-prettier"; // Import Prettier plugin
+import prettier from "eslint-config-prettier";
 
 export default [
-  {
-    languageOptions: { 
-      globals: globals.browser 
-    }
-  },
-  pluginJs.configs.recommended, // ESLint recommended rules
-  prettierConfig, // Include Prettier config directly
-  {
-    plugins: {
-      prettier: prettierPlugin, // Register the Prettier plugin
-    },
-    rules: {
-      ...prettierPlugin.configs.recommended.rules, // Use Prettier's recommended rules
-      'prettier/prettier': 'error', // Make Prettier formatting an error
-    }
-  }
+  pluginJs.configs.recommended,
+  prettier,
 ];
